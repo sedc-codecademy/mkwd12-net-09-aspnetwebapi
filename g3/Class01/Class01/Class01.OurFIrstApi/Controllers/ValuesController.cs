b036e2f4758088150e2e8fad34e8e3f1fcc32336
Martin <panovski.martin93@gmail.com>
2024-08-05 20:53:41 +0200

    Class 01 demo code added

---
+++ g3/Class01/Class01/Class01.OurFIrstApi/Controllers/ValuesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Class01.OurFIrstApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
