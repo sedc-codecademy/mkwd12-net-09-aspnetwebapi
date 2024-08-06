e85e013fec3d2490aff111ff8bab50759aed6f48
Ilija Mitev <94140253+ilijamitev@users.noreply.github.com>
2024-08-06 20:29:53 +0200

    Class 01 finished

---
+++ g2/Class01/OurFirstWebApi/OurFirstWebApi/Controllers/StudentsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OurFirstWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        // api/students
+        public List<string> Get()
+        {
+            var students = new List<string>() { "Bob Bobsky", "John Doe", "Jane Doe", "Greg Gregsky" };
+            return students;
+        }
+
+        [HttpGet("grades")]
+        // api/students/grades
+        public List<int> GetStudentsGrades()
+        {
+            return new List<int>() { 1, 5, 3 };
+        }
+    }
+}
