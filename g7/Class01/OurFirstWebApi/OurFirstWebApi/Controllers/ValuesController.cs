79f7ef4b66dbc10f827c77a33b644e9ddd6acf3c
tpelivanov <t.pelivanov@linkplus-it.com>
2024-08-06 20:43:57 +0200

    Class01

---
+++ g7/Class01/OurFirstWebApi/OurFirstWebApi/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OurFirstWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("string")]
+        public IEnumerable<string> GetStrings(string inputValue)
+        {
+            return new List<string> { "value1", "value2", "value34", inputValue };
+        }
+    }
+}
