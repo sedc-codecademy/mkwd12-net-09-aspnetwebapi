046e50081ffca1bfaeccb1d95acda17a76e87f77
Ilija Mitev <94140253+ilijamitev@users.noreply.github.com>
2024-08-13 18:34:01 +0200

    Class 03 init

---
+++ g7/Class03/Qinshift.NotesApp/Qinshift.NotesApp/Models/Note.cs
@@ -0,0 +1,11 @@
+﻿using Qinshift.NotesApp.Models.Enums;
+
+namespace Qinshift.NotesApp.Models
+{
+    public class Note
+    {
+        public string Text { get; set; }
+        public Priority Priority { get; set; }
+        public List<Tag> Tags { get; set; } = new List<Tag>();
+    }
+}
