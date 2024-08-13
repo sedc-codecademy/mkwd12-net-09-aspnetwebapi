cb40ca8eb603d2f462f501654bab59ba0097b478
Martin <panovski.martin93@gmail.com>
2024-08-13 08:55:02 +0200

    Class 04 API project code added

---
+++ g3/Class04/Qinshift.EShop/Qinshift.EShop.API/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Qinshift.EShop.API.Models
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
---
+++ g3/Class04/Qinshift.EShop/Qinshift.EShop.API/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Qinshift.EShop.API.Models
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+        public string ImageUrl { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public IEnumerable<Review> Reviews { get; set; }
+    }
+}
