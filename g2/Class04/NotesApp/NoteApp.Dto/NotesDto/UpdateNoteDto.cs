3524b36982bdef8c1d81573593b2989b0813ed19
Danilo Borozan <daniloborozan07@gmail.com>
2024-08-15 20:49:26 +0200

    add code

---
+++ g2/Class04/NotesApp/NoteApp.Dto/NotesDto/UpdateNoteDto.cs
@@ -0,0 +1,18 @@
+﻿using NotesApp.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesApp.Dto.NotesDto
+{
+    public class UpdateNoteDto
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public Priority Priority { get; set; }
+        public int UserId { get; set; }
+        public List<int> TagId { get; set; }
+    }
+}
