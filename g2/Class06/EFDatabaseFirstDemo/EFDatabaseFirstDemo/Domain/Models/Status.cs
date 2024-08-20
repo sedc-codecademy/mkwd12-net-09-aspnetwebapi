57dc467f3605af68b97051fb840719c081358abf
Ilija Mitev <94140253+ilijamitev@users.noreply.github.com>
2024-08-20 18:33:34 +0200

    Class 06 update

---
+++ g2/Class06/EFDatabaseFirstDemo/EFDatabaseFirstDemo/Domain/Models/Status.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFDatabaseFirstDemo.Domain.Models
+{
+    public partial class Status
+    {
+        public Status()
+        {
+            Todos = new HashSet<Todo>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+
+        public virtual ICollection<Todo> Todos { get; set; }
+    }
+}
---
+++ g2/Class06/NotesApp/NotesApp.Domain/Models/Note.cs
@@ -0,0 +1,13 @@
+﻿using NotesApp.Domain.Enums;
+
+namespace NotesApp.Domain.Models
+{
+    public class Note : BaseEntity
+    {
+        public string Text { get; set; }
+        public Priority Priority { get; set; }
+        public Tag Tag { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
