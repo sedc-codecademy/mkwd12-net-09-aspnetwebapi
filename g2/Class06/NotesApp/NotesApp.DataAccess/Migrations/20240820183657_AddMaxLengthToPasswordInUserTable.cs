2b5c1c9161786d0287ea74cc555b2bad2dc0962e
Ilija Mitev <94140253+ilijamitev@users.noreply.github.com>
2024-08-20 20:45:33 +0200

    Class 06 finished

---
+++ g2/Class06/NotesApp/NotesApp.DataAccess/Migrations/20240820183657_AddMaxLengthToPasswordInUserTable.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NotesApp.DataAccess.Migrations
+{
+    public partial class AddMaxLengthToPasswordInUserTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "User",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000);
+        }
+    }
+}
