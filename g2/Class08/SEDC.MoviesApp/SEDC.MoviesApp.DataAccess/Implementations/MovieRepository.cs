4eca51c2e4661eec4a238d18e12db1029c35f333
Danilo Borozan <daniloborozan07@gmail.com>
2024-08-27 20:29:47 +0200

    add

---
+++ g2/Class08/SEDC.MoviesApp/SEDC.MoviesApp.DataAccess/Implementations/MovieRepository.cs
@@ -0,0 +1,65 @@
+﻿using SEDC.MoviesApp.Domain;
+using SEDC.MoviesApp.Shared;
+
+namespace SEDC.MoviesApp.DataAccess
+{
+    public class MovieRepository : IMovieRepository
+    {
+        private readonly MoviesDbContext _context;
+        public MovieRepository(MoviesDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Movie entity)
+        {
+            _context.Movies.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(Movie entity)
+        {
+            _context.Movies.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public List<Movie> FilterMovies(int? year, GenreEnum? genre)
+        {
+            if (genre == null && year == null)
+            {
+                return _context.Movies.ToList();
+            }
+
+
+            if (year == null)
+            {
+                List<Movie> moviesDb = _context.Movies.Where(x => x.Genre == (GenreEnum)genre).ToList();
+                return moviesDb;
+            }
+            if (genre == null)
+            {
+                List<Movie> moviesDb = _context.Movies.Where(x => x.Year == year).ToList();
+                return moviesDb;
+            }
+            List<Movie> movies = _context.Movies.Where(x => x.Year == year
+                                                         && x.Genre == (GenreEnum)genre).ToList();
+            return movies;
+        }
+
+        public IEnumerable<Movie> GetAll()
+        {
+            return _context.Movies;
+        }
+
+        public Movie GetById(int id)
+        {
+            return _context.Movies.SingleOrDefault(x => x.Id == id);
+        }
+
+        public void Update(Movie update)
+        {
+            _context.Movies.Update(update);
+            _context.SaveChanges();
+        }
+    }
+}
