2f3395dcd57bfd4049ff089448141ba5b07ea027
tpelivanov <t.pelivanov@linkplus-it.com>
2024-08-27 20:49:58 +0200

    Class08

---
+++ g7/Class08/Qinshift.NotesAppRefactored/Qinshift.NotesAppRefactored.Dtos/NoteDtos/AddNoteDto.cs
@@ -0,0 +1,12 @@
+﻿using Qinshift.NotesAppRefactored.Domain.Enums;
+
+namespace Qinshift.NotesAppRefactored.Dtos.NoteDtos
+{
+    public class AddNoteDto
+    {
+        public string Text { get; set; }
+        public Priority Priority { get; set; }
+        public Tag Tag { get; set; }
+        public int UserId { get; set; }
+    }
+}
