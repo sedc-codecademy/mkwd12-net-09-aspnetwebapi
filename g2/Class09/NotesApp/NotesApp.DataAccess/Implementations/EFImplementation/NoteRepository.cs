c02d266e3964366c20b0699a447df69cd23b8f48
Ilija Mitev <94140253+ilijamitev@users.noreply.github.com>
2024-08-29 17:29:26 +0200

    Class 09 Init

---
+++ g2/Class09/NotesApp/NotesApp.DataAccess/Implementations/EFImplementation/NoteRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotesApp.DataAccess.Context;
+using NotesApp.Domain.Models;
+
+namespace NotesApp.DataAccess.Implementations.EFImplementation
+{
+    public class NoteRepository : IRepository<Note>
+    {
+        private readonly NotesAppDbContext _notesAppDbContext;
+
+        public NoteRepository(NotesAppDbContext notesAppDbContext)
+        {
+            _notesAppDbContext = notesAppDbContext;
+        }
+
+        public List<Note> GetAll()
+        {
+            return _notesAppDbContext.Note
+                .Include(x => x.User) // Join Notes table with Users table
+                .ToList();
+        }
+
+        public Note GetById(int id)
+        {
+            return _notesAppDbContext.Note.Include(x => x.User).FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Add(Note entity)
+        {
+            _notesAppDbContext.Note.Add(entity);
+            _notesAppDbContext.SaveChanges();
+        }
+
+        public void Update(Note entity)
+        {
+            _notesAppDbContext.Note.Update(entity);
+            _notesAppDbContext.SaveChanges();
+        }
+
+        public void Delete(Note entity)
+        {
+            _notesAppDbContext.Note.Remove(entity);
+            _notesAppDbContext.SaveChanges();
+        }
+
+    }
+}
---
+++ g2/Class09/NotesApp/NotesApp.DataAccess/Implementations/EFImplementation/UserRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotesApp.DataAccess.Context;
+using NotesApp.Domain.Models;
+
+namespace NotesApp.DataAccess.Implementations.EFImplementation
+{
+    public class UserRepository : IRepository<User>
+    {
+        private NotesAppDbContext _notesAppDbCotext;
+
+        public UserRepository(NotesAppDbContext notesAppDbCotext)
+        {
+            _notesAppDbCotext = notesAppDbCotext;
+        }
+
+        public List<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetById(int id)
+        {
+            return _notesAppDbCotext.User.Include(x => x.Notes).FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Add(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
